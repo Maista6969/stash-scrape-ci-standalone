269125d87b4fda8e83a47f383c1c4f10ddae17fd
Maista <glad.beach5807@fastmail.com>
2026-08-29 22:32:54 +0200

    Fix typo

--- public/assets/scene.js
+++ public/assets/scene.js
@@ -8,7 +8,7 @@ function replaceData(data) {
   // complex replacements
   replaceResult(data.result?.performers, "result-performers", data.result?.performers?.join(" | "))
   replaceResult(data.result?.studio, "result-studio", `${data.result?.studio} • ${data.result?.date}`)
-  if (data.results?.duration) replaceResult(data.result?.duration, "result-duration", new Date(data.result.duration * 1000).toISOString().substring(11, 16).replace("-", ":"))
+  if (data.result?.duration) replaceResult(data.result?.duration, "result-duration", new Date(data.result.duration * 1000).toISOString().substring(11, 16).replace("-", ":"))
 
   // manual replacements
   // add groups
